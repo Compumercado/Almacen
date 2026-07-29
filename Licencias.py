7f1a6738a0e3e508c0c858619fcf61c9c7f5e35c
Compumercado <compumercadojdiaz@gmail.com>
2026-07-29 15:06:21 -0600

    Add files via upload

---
+++ Licencias.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Esto se mostrará en la interfaz web de Streamlit
+st.title("¡Hola, mundo desde Streamlit!")
+st.write("Este es mi primer programa interactivo.")
+print("¡Hola, mundo!")
